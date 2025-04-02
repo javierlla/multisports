b372aed210c4f34538cc2906c015dec1738985a4
javierlla <llarena.1999@gmail.com>
2025-04-02 17:23:19 +0200

    css añadido

--- JS/dom.js
+++ JS/dom.js
@@ -3,7 +3,10 @@ function createCard(title, properties)
 {
     const card = document.createElement('div');
     card.classList.add('card');
-
+    
+/*     const fav = document.createElement('button');
+    fav.classList.add('favorite-btn');
+    card.appendChild(fav); */
     const titleElement = document.createElement('h3');
     titleElement.textContent = title;
     card.appendChild(titleElement);
@@ -22,7 +25,7 @@ function displayData(containerId, data, createItem)
     const container = document.getElementById(containerId);
     if (!container) return;
 
-    container.innerHTML = ''; // Limpiar antes de agregar
+    container.innerHTML = ''; 
     data.forEach(item => container.appendChild(createItem(item)));
 }
 
